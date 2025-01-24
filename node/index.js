905b8c16afdd3196d0465bb8d4a932106fcaea55
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2025-01-24 20:41:42 +0530

    dynamic port added to backend

--- node/index.js
+++ node/index.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import authRoutes from './routes/auth.js';
-import eventRoutes from './routes/events.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+import eventRoutes from "./routes/events.js";
 import cloudinaryRoutes from "./routes/cloudinary.js";
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
@@ -15,10 +14,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*', // Allow all origins for simplicity (restrict in production)
+    origin: "*", // Allow all origins for simplicity (restrict in production)
   },
 });
-const PORT = process.env.VITE_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -29,28 +28,25 @@ app.use((req, res, next) => {
   next();
 });
 
-
 // Routes
 
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
 app.use("/api/cloudinary", cloudinaryRoutes);
 
-
-
 // Handle socket connections
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
-
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
